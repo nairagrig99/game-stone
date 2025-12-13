d16227345df31f451a1584ee23c7d877c6cad338
nairagrig99 <nairagrig1999@gmail.com>
2025-12-13 19:35:36 +0400

    final result

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import {COLORS} from "./constants.js";
 
-const scope = generateRandomColors();
+let scope = generateRandomColors();
 
 function generateRandomColors() {
     const colors = []
@@ -20,9 +20,14 @@ function generateRandomColors() {
     return colors;
 }
 
+function generateColorForRemovedElement() {
+
+}
+
 console.log('scope', scope);
 
 const clickHandler = selectFirstElement();
+
 drawStone();
 
 function drawStone() {
@@ -36,6 +41,11 @@ function drawStone() {
             const innerColors = createDOMElement('p')
             innerBorder.classList.add('color-border');
             innerColors.classList.add('color-item');
+
+            if (scope[i][j].color === undefined) {
+                console.log("hereee")
+                return
+            }
 
             innerColors.style.backgroundColor = scope[i][j].color;
             if (scope[i][j].selected) {
@@ -57,35 +67,131 @@ function createDOMElement(tagNAME) {
 }
 
 function selectFirstElement() {
-    // let selected = false;
-
     return (currentStone, event) => {
 
-        const selected = scope.flat().find((stone) => {
+        const selectedStone = scope.flat().find((stone) => {
             return stone.selected
         });
 
-        if (!selected) {
+        if (!selectedStone) {
             currentStone.selected = true;
             event.target.parentElement.classList.add("selected-item");
         } else {
+
+
+            if (!isNeighbors(selectedStone, currentStone)) {
+                selectedStone.selected = false;
+                currentStone.selected = true;
+                event.target.parentElement.classList.add("selected-item");
+                updateView();
+                return
+            }
+
             const tmpColor = currentStone.color;
-            currentStone.color = selected.color
-            selected.color = tmpColor;
+            currentStone.color = selectedStone.color
+            selectedStone.color = tmpColor;
 
-            currentStone.selected = true;
-            selected.selected = false;
+            const matchedStones = isColorMatches(currentStone, currentStone.positionY, currentStone.positionX);
 
-            document.querySelector('#square').innerHTML = "";
-            drawStone()
+            if (matchedStones.length >= 3) {
+
+
+                matchedStones.forEach(stone => {
+                    stone.color = 'black';
+                });
+
+                fillEmptyStones(matchedStones)
+
+                currentStone.selected = true;
+                selectedStone.selected = false;
+
+                updateView();
+
+            }
         }
-        console.log("stone", scope)
     }
 }
 
-// function replacePlacesOfStone() {
-//
-// }
-//
-// const secondStonePlace = replacePlacesOfStone(2)
-// secondStonePlace(4);
+function isNeighbors(selectedStone, currentStone) {
+
+    const xLineIsEqual = selectedStone.positionX === currentStone.positionX;
+    const yLineIsEqual = selectedStone.positionY === currentStone.positionY;
+
+    const directionForMoving = {
+        up: selectedStone.positionY - 1 === currentStone.positionY && xLineIsEqual,
+        down: selectedStone.positionY + 1 === currentStone.positionY && xLineIsEqual,
+        left: selectedStone.positionX - 1 === currentStone.positionX && yLineIsEqual,
+        right: selectedStone.positionX + 1 === currentStone.positionX && yLineIsEqual
+    }
+
+    return Object.values(directionForMoving).some((el) => el);
+}
+
+function isColorMatches(selectedStone, positionY, positionX, matchedStones = []) {
+    // debugger;
+    // const inner = () => {
+    if (positionY < 0 || positionY >= scope.length || positionX < 0 || positionX >= scope[0].length) {
+        return matchedStones;
+    }
+
+    const currentStone = scope[positionY][positionX];
+    if (currentStone.color !== selectedStone.color || matchedStones.includes(currentStone)) {
+        return matchedStones;
+    }
+    matchedStones.push(currentStone);
+
+    // check up and down stones color matches
+    isColorMatches(selectedStone, positionY - 1, positionX, matchedStones);
+    isColorMatches(selectedStone, positionY + 1, positionX, matchedStones);
+
+    // check left and right stones color matches
+    isColorMatches(selectedStone, positionY, positionX - 1, matchedStones);
+    isColorMatches(selectedStone, positionY, positionX + 1, matchedStones);
+
+    return matchedStones;
+    // }
+
+
+}
+
+function updateView() {
+    document.querySelector('#square').innerHTML = "";
+    drawStone()
+}
+
+function fillEmptyStones(removedStones) {
+
+    removedStones.forEach(stone => {
+        const {positionX, positionY} = stone;
+        scope[positionY][positionX].color = 'black';
+    });
+
+    for (let x = 0; x < scope[0].length; x++) {
+        let writePos = scope.length - 1;
+
+        for (let y = scope.length - 1; y >= 0; y--) {
+            if (scope[y][x].color !== 'black') {
+                if (writePos !== y) {
+                    scope[writePos][x] = scope[y][x];
+                    scope[writePos][x].positionY = writePos;
+                }
+                writePos--;
+            }
+        }
+
+        for (let y = writePos; y >= 0; y--) {
+            const randomColorIndex = Math.floor(Math.random() * COLORS.length);
+            scope[y][x] = {
+                color: COLORS[randomColorIndex],
+                selected: false,
+                positionX: x,
+                positionY: y
+            };
+        }
+    }
+
+    updateView();
+}
+
+
+
